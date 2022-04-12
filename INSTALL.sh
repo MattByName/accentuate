e11177d60f6086590d2d83d2d8aac67dcdec6e45
Matt Barnard <contact@missingbit.co.uk>
2022-04-12 21:21:14 +0100

    getCharacters can now pull the array of accented characters

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+mkdir ~/.local/share/gnome-shell/extensions/accentuate\@contact.missingbit.co.uk
 unzip accentuate\@contact.missingbit.co.uk.zip -du ~/.local/share/gnome-shell/extensions/accentuate\@contact.missingbit.co.uk
--- src/extension.js
+++ src/extension.js
@@ -4,10 +4,41 @@
  */
 
 /* exported init */
+const Gio = imports.gi.Gio;
+const ShellVersion = imports.misc.config.PACKAGE_VERSION.split('.');
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
+let ExtensionPath;
+if (ShellVersion[1] === 2) {
+    ExtensionPath = ExtensionSystem.extensionMeta['colortint@matt.serverus.co.uk'].path;
+} else {
+    ExtensionPath = imports.misc.extensionUtils.getCurrentExtension().path;
+}
 
+function getCharacters(search_char) {
+    let characters = loadCharacters()
+    for (const character of characters)
+	if (character.name == search_char)
+	    return character.accented
+    }
+
+function loadCharacters() {
+    file = Gio.file_new_for_path(ExtensionPath + '/characters.json');
+    [flag, data] = file.load_contents(null);
+
+    if (flag) {
+        const ByteArray = imports.byteArray;
+        let prepData = (data instanceof Uint8Array) ? ByteArray.toString(data) : data.toString();
+        characters = JSON.parse(prepData);
+    }
+    return characters
+}
+
+function getCharacterKey() {
+    
+
+}
 
 function init(meta) {
     log(`initializing ${meta.metadata.name}`);
@@ -16,6 +47,7 @@ function init(meta) {
 
 function enable() {
     log(`enabling ${Me.metadata.name}`);
+    log(getCharacters("a"))
     //activate keybinds
 }
 
