c57ee69d40d2c0204365ca2913b96b11633f4e55
Matt Barnard <matt@serverus.co.uk>
2022-04-12 14:21:13 +0100

    added skeleton repository

---
+++ INSTALL.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+unzip accentuate\@contact.missingbit.co.uk.zip -du ~/.local/share/gnome-shell/extensions/accentuate\@contact.missingbit.co.uk
---
+++ PACKAGE.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Create a zip file with source and meta files
+zip -j accentuate\@contact.missingbit.co.uk.zip src/* CHANGELOG.md LICENSE README.md
